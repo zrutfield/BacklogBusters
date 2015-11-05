84b0657f3e72373416d4da069186ba8247173629
Peter Kassimis <kassip@rpi.edu>
2015-11-05 17:34:21 -0500

    Search Update
    Added searching the database by game name. Other search methods still
    need to be added.

--- Skeleton Prototype/stats.php
+++ Skeleton Prototype/stats.php
@@ -1,5 +1,30 @@
 <?php 
 	require_once("header.php");
+
+	if (isset($_POST['gamename']) && $_POST['gamename'])
+	{
+		$stmt=$dbconn->prepare("SELECT * FROM `games` WHERE `gameName`=:gn");
+		$gn=$_POST["gamename"];
+		$stmt->execute(array(':gn'=>$gn));
+		$results = $stmt->fetch();
+		print_r($results);
+	}
+	else if (isset($_POST['sessiontime']) && $_POST['sessiontime'])
+	{
+		$stmt=$dbconn->prepare("SELECT * FROM `games` WHERE `gamename`=:gn");
+		$gn=$_POST["gamename"];
+		$stmt->execute(array(':gn'=>$gn));
+		$results = $stmt->fetch();
+		print_r($results);
+	}
+	else if (isset($_POST['completiontime']) && $_POST['completiontime'])
+	{
+		$stmt=$dbconn->prepare("SELECT * FROM `games` WHERE `gamename`=:gn");
+		$gn=$_POST["gamename"];
+		$stmt->execute(array(':gn'=>$gn));
+		$results = $stmt->fetch();
+		print_r($results);
+	}
 ?>
 
 <form name="searchname" action="stats.php" method="post">
