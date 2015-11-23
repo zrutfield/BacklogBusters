403320ca833c6d1f5c8028a7a102f6f3d1ac0367
Peter Kassimis <kassip@rpi.edu>
2015-11-23 18:38:46 -0500

    Adding Comments
    Commenting several pages, more to come.

--- Skeleton Prototype/header.php
+++ Skeleton Prototype/header.php
@@ -1,10 +1,12 @@
 <?php
+	//Establish the database connection.
 	try 
 	{
 	  $host = 'localhost';
 	  $root = 'root';
 	  $password = '';
 
+	  //PDOs are a safe and secure method of accessing a database.
 	  $dbconn = new PDO("mysql:host=$host;dbname=backlogbusters;",$root,$password);
 
 	} catch (PDOException $e) 
@@ -22,6 +24,7 @@
 
         <body>
                 <?php
+                #Display the login/register or logout button(s) depending on if the user is logged in or not.
                 if($_SESSION['userid'])
                 {
                     echo '<div id="login">Logged In ' . $_SESSION['userid'] . '</div>';
@@ -31,11 +34,11 @@
 
             
 		<h1>BacklogBusters</h1>
+		<!--The menu bar that tops every page.-->
 		<ul id="menulist">
 			<li><a href="index.php">Home</a></li>
 			<li><a href="account.php">Account</a></li>
 			<li><a href="calendar.php">Calendar</a></li>
-			<!--<li><a href="schedule.php">Schedule</a></li>-->
 			<li><a href="dataentry.php">Enter Data</a></li>
 			<li><a href="stats.php">Stats</a></li>
 		</ul>
