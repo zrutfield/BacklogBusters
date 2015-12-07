8e3fb1d353247b4f8e0f719969b25eedfc63315f
Peter Kassimis <kassip@rpi.edu>
2015-12-07 13:49:09 -0500

    Index Page Update
    added some more substance to the landing page.

--- Skeleton Prototype/index.php
+++ Skeleton Prototype/index.php
@@ -4,9 +4,12 @@
 
 <!--Obviously this is all placeholder; this would be filled with front-page content once it's actually decided what's worthy of being front-page content.-->
 
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam cursus nibh elementum nibh condimentum, id congue odio varius. Pellentesque eleifend mauris quis dignissim dictum. Pellentesque consectetur dictum velit non accumsan. Aenean tempus orci odio, ut laoreet erat egestas nec. In eu tristique tortor. Cras in dictum magna. Morbi tempor at libero quis tincidunt. Etiam vulputate elit nec dui sodales luctus. Nulla facilisi.</p>
+<h2>Welcome to Backlog Busters!</h2>
+<p>This is an online resource for fitting gaming into your schedule. To get started, register an account and then connect it to your Steam profile via the Account page.</h3>
+<p>Once we've loaded your game library, you can set up game times on the Calendar page to get our recommendations!</p>
 
-<p>Duis nisl nibh, aliquam ac tincidunt a, volutpat in justo. Cras sollicitudin blandit varius. Quisque euismod risus in lobortis vulputate. Cras purus turpis, fermentum ac diam sed, convallis malesuada sapien. Proin non condimentum ante. Vestibulum dapibus leo ut ipsum tempor, venenatis iaculis turpis vehicula. Curabitur pharetra non lacus at dignissim. Suspendisse lacinia vel lectus in maximus. Vivamus varius enim metus, non eleifend ante vehicula quis. Vivamus non purus finibus, fermentum velit ac, ultricies neque. Suspendisse et rutrum leo. Donec placerat congue rhoncus. Donec eget odio efficitur, tincidunt massa eget, posuere nulla. Donec mollis metus dui, sed suscipit massa rhoncus eu. Phasellus efficitur malesuada dolor at varius. Nam viverra ex turpis, nec laoreet ante euismod vel.</p>
+<h2>News:</h2>
+<p>Backlog Busters Beta Release! The website is still an evolving project, but our resources are now publicly available for anyone who wants to give them a try.</p>
 
 <?php
 	require_once("footer.php");
